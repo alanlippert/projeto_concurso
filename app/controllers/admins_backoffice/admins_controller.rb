30a9dd5ea3a1858f4253e38dfc834726a1e72d2e
alanlippert <alanlippert@hotmail.com>
2023-10-15 23:53:31 -0300

    Finalizando controllers e Views

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,22 +1,31 @@
-# frozen_string_literal: true
-
 module AdminsBackoffice
   class AdminsController < AdminsBackofficeController
+    before_action :verify_password, only: %i[update]
     before_action :set_admin, only: %i[show edit update destroy]
-
+   
     def index
       @admins = Admin.all
     end
 
+    def new
+      @admin = Admin.new
+    end
+
+    def create
+      @admin = Admin.new(params_admin)  
+      if @admin.save
+        redirect_to admins_backoffice_admins_path, notice: 'Admin cadastrado com sucesso.', status: :see_other
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+    
     def show; end
 
     def edit; end
 
-    def update
-      if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
-        params[:admin].extract!(:password, :password_confirmation)
-      end
-      if @admin.update(admin_params)
+    def update            
+      if @admin.update(params_admin)
         respond_to do |format|
           format.html { redirect_to admins_backoffice_admins_path, notice: 'Admin atualizado com sucesso.' }
           format.turbo_stream
@@ -37,8 +46,14 @@ module AdminsBackoffice
       @admin = Admin.find(params[:id])
     end
 
-    def admin_params
+    def params_admin
       params.require(:admin).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+    
+    def verify_password
+      if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+        params[:admin].extract!(:password, :password_confirmation)
+      end
     end
   end
 end

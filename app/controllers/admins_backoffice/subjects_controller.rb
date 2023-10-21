43acdb89389b83605d86a23a1186971e6dd6bd2a
alanlippert <alanlippert@hotmail.com>
2023-10-21 12:36:40 -0300

    update controller subject

--- app/controllers/admins_backoffice/subjects_controller.rb
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -1,5 +1,7 @@
 module AdminsBackoffice
   class AdminsBackoffice::SubjectsController < AdminsBackofficeController
+    before_action :set_subject, only: %i[show edit update destroy]
+
     def index
       @subjects = Subject.all
     end
@@ -9,7 +11,31 @@ module AdminsBackoffice
     end
 
     def create
-      @subject = Subject.create(subject_params)      
+      @subject = Subject.create(subject_params)
+      if @subject.save
+        redirect_to admins_backoffice_subject_path, notice: 'Assunto cadastrado com sucesso.', status: :see_other
+      else
+        render :new, status: :unprocessable_entity
+      end 
+    end
+
+    def show
+    end
+    
+    def edit
+    end
+
+    def update
+      if @subject.update(params_subject)
+        redirect_to admins_backoffice_subject_path, notice: 'Assunto atualizado com sucesso.' 
+      else
+        render :edit, status: :unprocessable_entity 
+      end
+    end
+
+    def destroy
+      @subject.destroy
+       redirect_to admins_backoffice_subject_path, notice: 'Assunto atualizado com sucesso.' 
     end
 
 

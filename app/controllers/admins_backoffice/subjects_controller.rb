51a78cb45ac8f7d575e8998d22b8e909f6c52294
alanlippert <alanlippert@hotmail.com>
2023-10-21 11:44:00 -0300

    create controller subject

--- app/controllers/admins_backoffice/subjects_controller.rb
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -3,5 +3,24 @@ module AdminsBackoffice
     def index
       @subjects = Subject.all
     end
+    
+    def new
+      @subject = Subject.new
+    end
+
+    def create
+      @subject = Subject.create(subject_params)      
+    end
+
+
+    private
+    
+    def set_subject
+      @subject = Subject.find(params[:id])
+    end
+
+    def params_subject
+      params.require(:subject).permit(:description)
+    end
   end
 end

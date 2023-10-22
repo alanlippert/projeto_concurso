b55c413e8f6c62185835c7a31e6cb7d1edb4355f
alanlippert <alanlippert@hotmail.com>
2023-10-22 00:46:52 -0300

    update

--- app/controllers/admins_backoffice/subjects_controller.rb
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -1,53 +1,52 @@
 # frozen_string_literal: true
 
 module AdminsBackoffice
-  module AdminsBackoffice
-    class SubjectsController < AdminsBackofficeController
-      before_action :set_subject, only: %i[show edit update destroy]
+  class SubjectsController < AdminsBackofficeController
+    before_action :set_subject, only: %i[show edit update destroy]
 
-      def index
-        @subjects = Subject.all.page(params[:page]).per(10)
+    def index
+      @subjects = Subject.all.page(params[:page]).per(10)
+    end
+
+    def new
+      @subject = Subject.new
+    end
+
+    def create
+      @subject = Subject.new(params_subject)
+      if @subject.save
+        redirect_to admins_backoffice_subjects_path, notice: 'Assunto cadastrado com sucesso.', status: :see_other
+      else
+        render :new, status: :unprocessable_entity
       end
+    end
 
-      def new
-        @subject = Subject.new
+    def show; end
+
+    def edit; end
+
+    def update
+      if @subject.update(params_subject)
+        redirect_to admins_backoffice_subjects_path, notice: 'Assunto atualizado com sucesso.'
+      else
+        render :edit, status: :unprocessable_entity
       end
+    end
 
-      def create
-        @subject = Subject.new(params_subject)
-        if @subject.save
-          redirect_to admins_backoffice_subjects_path, notice: 'Assunto cadastrado com sucesso.', status: :see_other
-        else
-          render :new, status: :unprocessable_entity
-        end
-      end
+    def destroy
+      @subject.destroy
+      redirect_to admins_backoffice_subject_path, notice: 'Assunto atualizado com sucesso.'
+    end
 
-      def show; end
+    private
 
-      def edit; end
+    def set_subject
+      @subject = Subject.find(params[:id])
+    end
 
-      def update
-        if @subject.update(params_subject)
-          redirect_to admins_backoffice_subjects_path, notice: 'Assunto atualizado com sucesso.'
-        else
-          render :edit, status: :unprocessable_entity
-        end
-      end
-
-      def destroy
-        @subject.destroy
-        redirect_to admins_backoffice_subject_path, notice: 'Assunto atualizado com sucesso.'
-      end
-
-      private
-
-      def set_subject
-        @subject = Subject.find(params[:id])
-      end
-
-      def params_subject
-        params.require(:subject).permit(:description)
-      end
+    def params_subject
+      params.require(:subject).permit(:description)
     end
   end
 end
+

f9fc7093970e8c8afb7d4e4edcf3580183c143ab
alanlippert <alanlippert@hotmail.com>
2023-10-30 21:39:40 -0300

    update models

--- app/controllers/site/welcome_controller.rb
+++ app/controllers/site/welcome_controller.rb
@@ -3,7 +3,7 @@
 module Site
   class WelcomeController < SiteController
     def index
-      @questions = Question.all.includes(:answers).page(params[:page]).per(params[:page])
+      @questions = Question.all.includes(:answers).order(created_at: :desc).page(params[:page]).per(params[:page])
     end
   end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -7,4 +7,9 @@ class Admin < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, uniqueness: true
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
 end
